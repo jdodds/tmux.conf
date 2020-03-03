85cbc57a9f484c63e97da947d7fa128c048775a4
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2020-03-03 10:56:48 -0500

    use a better wait/kill method and restore old pane settings

--- tmux-color-ssh.sh
+++ tmux-color-ssh.sh
@@ -2,14 +2,6 @@
 
 [[ -z "$TMUX_PANE" ]] && exit
 
-anywait() {
-    for pid in "$@"; do
-        while kill -0 "$pid"; do
-            sleep 0.5
-        done
-    done
-}
-
 
 colors=(
     12 17 18 22 23 24 34 35 36 37 38 39 52 53
@@ -18,8 +10,9 @@ colors=(
 
 c=${colors[$RANDOM % ${#colors[@]}]}
 
+current=$(tmux select-pane -g -t "$TMUX_PANE")
+
 tmux select-pane -P "bg=colour$c" -t "$TMUX_PANE"
 
-( anywait "$PPID"
+( while kill -0 "$PPID" 2> /dev/null; do sleep 1; done
   tmux select-pane -P "$current" -t "$TMUX_PANE" ) &
-
